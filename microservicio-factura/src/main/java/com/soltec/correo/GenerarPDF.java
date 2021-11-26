ae1cf9200280d67a0b2edf6c4c9a97cc690310c3
Sebasan07 <juansebastiansp@ufps.edu.co>
2021-11-26 16:37:56 -0500

    enviar correo y generar pdf con las tablas de información

--- microservicio-factura/src/main/java/com/soltec/correo/GenerarPDF.java
+++ microservicio-factura/src/main/java/com/soltec/correo/GenerarPDF.java
@@ -3,6 +3,7 @@ package com.soltec.correo;
 import com.itextpdf.io.font.FontConstants;
 import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.color.Color;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.PageSize;
@@ -11,7 +12,9 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.property.TextAlignment;
+import com.soltec.entities.DetalleFactura;
 import com.soltec.entities.Factura;
 import com.soltec.entities.Usuario;
 
@@ -25,13 +28,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
+
 public class GenerarPDF {
 
     public GenerarPDF() {
 
     }
 
-    public boolean generarPDF(String nombreArchivo, File img, String nombreAnimal ,Usuario us, Factura fa) throws FileNotFoundException, IOException, InterruptedException {
+    public boolean generarPDF(String nombreArchivo, File img, String nombreAnimal ,Usuario us, Factura fa, DetalleFactura df) throws FileNotFoundException, IOException, InterruptedException {
 
         FileOutputStream f = new FileOutputStream(nombreArchivo);
 
@@ -50,6 +54,7 @@ public class GenerarPDF {
         	hue.setFontSize(36f);
         	System.out.println("n");
         	String[] fe=LocalDateTime.now().toString().split("T");
+        	Paragraph factura = new Paragraph("Factura Electrónica de Venta N° "+fa.getRangoNumeracionBean()).setFont(font1);
         	Paragraph fecha = new Paragraph("Fecha: "+fe[0]+" "+fe[1].substring(0,8)).setFont(font1);
         	Paragraph pTitulo = new Paragraph("Hola, "+us.getNombre() + " "+ us.getApellido()+"\n").setFont(font1);
         	Paragraph pCuerpo = new Paragraph("").setFont(font1);
@@ -57,10 +62,13 @@ public class GenerarPDF {
             Paragraph rolUser = new Paragraph("También se le hace saber que el usuario y usted pertenecen al rol de "+'"'+us.getRol()+'"'+", favor tener en cuenta lo anterior.").setFont(font);
             Paragraph despedida = new Paragraph("").setFont(font1);
             ImageData data = ImageDataFactory.create(img.getAbsolutePath());
-            System.out.println(data.toString());
             Image imga = new Image(data).setFontSize(28f); 
             // Adding paragraphs to document       
+            
+            Table table = new Table(2);
+            //proceso(table, font, document, burbuja, msBuble);
 
+            
             document.add(imga.setTextAlignment(TextAlignment.LEFT));
             document.add(hue);       
             document.add(fecha.setTextAlignment(TextAlignment.RIGHT));
@@ -92,4 +100,45 @@ public class GenerarPDF {
             JOptionPane.showMessageDialog(null, "No se abrirá el PDF");
         }
     }
+    
+    private void tablaProductos(Table tabla, PdfFont font, Document document, DetalleFactura df) {
+
+        tabla = new Table(6);//genera tabla
+        tabla.addHeaderCell("Productos").setBackgroundColor(Color.CYAN);//ENCABEZADO DE TABLA COLOR GRIS
+        tabla.addHeaderCell("Valor Unitario").setBackgroundColor(Color.CYAN);
+        tabla.addHeaderCell("Cantidad").setBackgroundColor(Color.CYAN);
+        tabla.addHeaderCell("Subtotal").setBackgroundColor(Color.CYAN);
+        tabla.addHeaderCell("Descuento").setBackgroundColor(Color.CYAN);
+        tabla.addHeaderCell("Total").setBackgroundColor(Color.CYAN);
+        
+        document.add(tabla);
+        tabla.setWidthPercent(90);
+        tabla = new Table(2);
+
+        for (int j = 0; j<6; j++) {
+            //condicionesUso(j, datosBurbujaBi, datosBubleSort, tabla, font);
+        }
+        cargarTablas(tabla, document);
+    }
+    
+    private void condicionesUso(int j, String datosBurbujaBi[], String datosBubleSort[], Table tabla, PdfFont font) {
+
+       
+        //tabla.addCell("agrega valor").setFont(font).setBackgroundColor(Color.YELLOW);
+        //tabla.addCell("").setFont(font).setBackgroundColor(Color.YELLOW);
+    }
+
+    private void cargarTablas(Table tabla, Document document) {
+        document.add(tabla);
+        tabla.setWidthPercent(90);
+
+        tabla = new Table(2);
+
+        //tabla.addFooterCell("").setBackgroundColor(Color.LIGHT_GRAY);//PIE DE PAGINA DE LA TABLA
+        //tabla.addFooterCell(" " ).setBackgroundColor(Color.LIGHT_GRAY);
+        document.add(tabla);
+        tabla.setWidthPercent(90);
+    }
+
+
 }
