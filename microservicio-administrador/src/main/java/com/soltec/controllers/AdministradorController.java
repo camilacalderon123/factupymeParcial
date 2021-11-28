9fd5c586752993a77e9bedf95a0953c656cffa5b
dubanyesid <dubanyesidsb@ufps.edu.co>
2021-11-27 23:30:59 -0500

    Administrador controllers arreglados

--- microservicio-administrador/src/main/java/com/soltec/controllers/AdministradorController.java
+++ microservicio-administrador/src/main/java/com/soltec/controllers/AdministradorController.java
@@ -1,10 +1,15 @@
 package com.soltec.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController // Controlador de tipo Rest
+@CrossOrigin(origins="http://localhost:4200", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT, RequestMethod.DELETE})
+@RequestMapping("/administrador")//Se accede a través de esta URL
 public class AdministradorController {
 	@RequestMapping("/")
     public String index() {
--- microservicio-administrador/src/main/java/com/soltec/controllers/EmpresaController.java
+++ microservicio-administrador/src/main/java/com/soltec/controllers/EmpresaController.java
@@ -9,17 +9,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.soltec.entities.Empresa;
 import com.soltec.service.EmpresaService;
 
 
-
+@RestController // Controlador de tipo Rest
+@CrossOrigin(origins="http://localhost:4200", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT, RequestMethod.DELETE})
+@RequestMapping("/empresa")//Se accede a través de esta URL
 public class EmpresaController {
 	
 	
