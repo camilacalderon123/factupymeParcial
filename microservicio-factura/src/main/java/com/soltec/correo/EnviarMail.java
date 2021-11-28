96b2842a3f4fac10da325dd748fba7fe664fcd33
Juan Sebastian Sanchez Prada <juansebastiansp@ufps.edu.co>
2021-11-28 00:49:41 -0500

    pdf y xml listos para enviar

--- microservicio-factura/src/main/java/com/soltec/correo/EnviarMail.java
+++ microservicio-factura/src/main/java/com/soltec/correo/EnviarMail.java
@@ -1,8 +1,14 @@
 package com.soltec.correo;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
 import javax.mail.Authenticator;
@@ -22,87 +28,89 @@ import com.aspose.pdf.SaveFormat;
 
 public class EnviarMail {
 
-    public boolean SendMail(Correo c) {
-        try {
-            Properties props = new Properties();
-            props.put("mail.smtp.host", "smtp.gmail.com");
-            props.setProperty("mail.smtp.starttls.enable", "true");
-            props.setProperty("mail.smtp.port", "587");
-            props.setProperty("mail.smtp.auth", "true");
-            Authenticator auth = new Authenticator() {
-                public PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(c.getUsuarioCorreo(), c.getContrasena());
-                }
-            };
+	public boolean SendMail(Correo c) {
+		try {
+			Properties props = new Properties();
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.setProperty("mail.smtp.starttls.enable", "true");
+			props.setProperty("mail.smtp.port", "587");
+			props.setProperty("mail.smtp.auth", "true");
+			Authenticator auth = new Authenticator() {
+				public PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(c.getUsuarioCorreo(), c.getContrasena());
+				}
+			};
 
-            Session s = Session.getDefaultInstance(props, auth);
-            BodyPart texto = new MimeBodyPart();
-            texto.setText(c.getMensaje());
-            BodyPart adjunto = new MimeBodyPart();
-            BodyPart adjunto1 = new MimeBodyPart();
-            
-            convertirAXML("recibo.pdf");
-            
-            if (!c.getRutaArchivo().equals("") &&!c.getRutaArchivo1().equals("")) {
-                adjunto.setDataHandler(new DataHandler(new FileDataSource(c.getRutaArchivo())));
-                adjunto.setFileName(c.getNombreArchivo());
-                System.out.println(c.getRutaArchivo());
-                System.out.println(c.getRutaArchivo1());
-                adjunto1.setDataHandler(new DataHandler(new FileDataSource(c.getRutaArchivo1())));
-                adjunto1.setFileName("recibo.xml");
-                System.out.println(c.getRutaArchivo1());
-            }
-            MimeMultipart m = new MimeMultipart();
-            m.addBodyPart(texto);
-            
-            if (!c.getRutaArchivo().equals("")&&!c.getRutaArchivo1().equals("")) {
-                m.addBodyPart(adjunto);
-                m.addBodyPart(adjunto1);
-            }
-            MimeMessage mensaje = new MimeMessage(s);
-            mensaje.setFrom(new InternetAddress(c.getUsuarioCorreo()));
-            InternetAddress[] toAddresses = { new InternetAddress(c.getDestino()) };
-            mensaje.setRecipients(Message.RecipientType.TO, toAddresses);
-           
-            // mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(c.getDestino()));
-            mensaje.setSubject(c.getAdjunto());
-            mensaje.setContent(m);
+			Session s = Session.getDefaultInstance(props, auth);
+			BodyPart texto = new MimeBodyPart();
+			texto.setText(c.getMensaje());
+			BodyPart adjunto = new MimeBodyPart();
+			BodyPart adjunto1 = new MimeBodyPart();
 
-			Transport.send(mensaje); 
+			convertirAXML("recibo.pdf");
 
-            return true;
+			if (!c.getRutaArchivo().equals("") && !c.getRutaArchivo1().equals("")) {
+				adjunto.setDataHandler(new DataHandler(new FileDataSource(c.getRutaArchivo())));
+				adjunto.setFileName(c.getNombreArchivo());
+				System.out.println(c.getRutaArchivo());
+				System.out.println(c.getRutaArchivo1());
+				adjunto1.setDataHandler(new DataHandler(new FileDataSource(c.getRutaArchivo1())));
+				adjunto1.setFileName("recibo.xml");
+				System.out.println(c.getRutaArchivo1());
+			}
+			MimeMultipart m = new MimeMultipart();
+			m.addBodyPart(texto);
 
-        } catch (Exception e) {
-        	System.out.println(e.getMessage());
-            return false;
-        }
-    }
+			if (!c.getRutaArchivo().equals("") && !c.getRutaArchivo1().equals("")) {
+				m.addBodyPart(adjunto);
+				m.addBodyPart(adjunto1);
+			}
+			MimeMessage mensaje = new MimeMessage(s);
+			mensaje.setFrom(new InternetAddress(c.getUsuarioCorreo()));
+			InternetAddress[] toAddresses = { new InternetAddress(c.getDestino()) };
+			mensaje.setRecipients(Message.RecipientType.TO, toAddresses);
 
-    private void convertirAXML(String nombreArchivo) {
-    	Document document = new Document(nombreArchivo);
-    	document.save("recibo.xml", SaveFormat.MobiXml);
-    	document.close();
-    }
-    
-    public void enviar(String destino, String mensaje) {
+			// mensaje.addRecipient(Message.RecipientType.TO, new
+			// InternetAddress(c.getDestino()));
+			mensaje.setSubject(c.getAdjunto());
+			mensaje.setContent(m);
 
-    	UUID uuid = UUID.randomUUID();
-        String uuidAsString = uuid.toString().split("-")[0];
-    	System.out.println("a");
-        String usuarioCorreo="facturacionpyme123@gmail.com";//correo
-        String aux="pyme12345";//contraseña
-        String nombreArchivo = "recibo.txt";
-        //String mensaje = "Deseamos hacerle llegar los datos del animal";
-        String adjunto = "FACTURACIÓN ELECTRÓNICA";
-        File destinoArchivo = new File(nombreArchivo);
-        String rutaArchivo = String.valueOf(destinoArchivo);
+			Transport.send(mensaje);
 
-        Correo c = new Correo(usuarioCorreo, nombreArchivo,nombreArchivo, aux, rutaArchivo,rutaArchivo, destino, mensaje, adjunto);
+			return true;
 
-        if (this.SendMail(c)) {
-            JOptionPane.showMessageDialog(null, "Mensaje Enviado");
-        } else {
-            JOptionPane.showMessageDialog(null, "Mensaje no Enviado");
-        }
-    }
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
+
+	private void convertirAXML(String nombreArchivo) {
+		Document document = new Document(nombreArchivo);
+		document.save("recibo.xml", SaveFormat.MobiXml);
+		document.close();
+	}
+
+	public void enviar(String destino, String mensaje) {
+
+		UUID uuid = UUID.randomUUID();
+		String uuidAsString = uuid.toString().split("-")[0];
+		System.out.println("a");
+		String usuarioCorreo = "facturacionpyme123@gmail.com";// correo
+		String aux = "pyme12345";// contraseña
+		String nombreArchivo = "recibo.txt";
+		// String mensaje = "Deseamos hacerle llegar los datos del animal";
+		String adjunto = "FACTURACIÓN ELECTRÓNICA";
+		File destinoArchivo = new File(nombreArchivo);
+		String rutaArchivo = String.valueOf(destinoArchivo);
+
+		Correo c = new Correo(usuarioCorreo, nombreArchivo, nombreArchivo, aux, rutaArchivo, rutaArchivo, destino,
+				mensaje, adjunto);
+
+		if (this.SendMail(c)) {
+			JOptionPane.showMessageDialog(null, "Mensaje Enviado");
+		} else {
+			JOptionPane.showMessageDialog(null, "Mensaje no Enviado");
+		}
+	}
 }
--- microservicio-factura/src/main/java/com/soltec/correo/Prueba.java
+++ microservicio-factura/src/main/java/com/soltec/correo/Prueba.java
@@ -20,26 +20,28 @@ public class Prueba {
 		File f = new File("soltec.png");
 
 		
-		  ec.enviarCorreo("recibo.pdf", f, new Cliente(1193530328, "SolTEC",
-		  "Angely Camila Calderón", "Colombia", "N. de Santander", "Cucúta",
-		  "Av 0 # 0-78", "angelycamilacg@ufps.edu.co", "3219810616", "Juridico",
-		  "Regimen", 1), new Factura("20211111SDSDFASDFA3424534", new
-		  GregorianCalendar(2021, 11, 11).getTime(), new GregorianCalendar(2025, 11,
-		  11).getTime(), 3, 45, (byte) 1, "SebastianSanchez", 1, 1, 1), new
-		  DetalleFactura(1, "CUFEDSA", 1, 1),
-		  new RangoNumeracion(24551, new Date(), 1876401818, 24551, 50000, 20001, "FE"));
+		/*
+		 * ec.enviarCorreo("recibo.pdf", f, new Cliente(1193530328, "SolTEC",
+		 * "Angely Camila Calderón", "Colombia", "N. de Santander", "Cucúta",
+		 * "Av 0 # 0-78", "angelycamilacg@ufps.edu.co", "3219810616", "Juridico",
+		 * "Regimen", 1), new Factura("20211111SDSDFASDFA3424534", new
+		 * GregorianCalendar(2021, 11, 11).getTime(), new GregorianCalendar(2025, 11,
+		 * 11).getTime(), 3, 45, (byte) 1, "SebastianSanchez", 1, 1, 1), new
+		 * DetalleFactura(1, "CUFEDSA", 1, 1), new RangoNumeracion(24551, new Date(),
+		 * 1876401818, 24551, 50000, 20001, "FE"));
+		 */
 		 
 
-			/*
-			 * GenerarPDF gp = new GenerarPDF(); gp.generarPDF("recibo.pdf", f, new
-			 * Cliente(1193530328, "SolTEC", "Angely Camila Calderón", "Colombia",
-			 * "N. de Santander", "Cucúta", "Av 0 # 0-78", "angelycamilcg@ufps.edu.co",
-			 * "3219810616", "Juridico", "Regimen", 1), new
-			 * Factura("CUFEASDSDFASDFA3424534", new GregorianCalendar(2021, 11,
-			 * 11).getTime(), new GregorianCalendar(2025, 11, 11).getTime(), 3, 45, (byte)
-			 * 1, "SebastianSanchez", 1, 1, 1), new DetalleFactura(1, "CUFEDSA", 1, 1), new
-			 * RangoNumeracion(24551, new Date(), 1876401818, 24551, 50000, 20001, "FE"));
-			 */
+			
+			  GenerarPDF gp = new GenerarPDF(); gp.generarPDF("recibo.pdf", f, new
+			  Cliente(1193530328, "SolTEC", "Angely Camila Calderón", "Colombia",
+			  "N. de Santander", "Cucúta", "Av 0 # 0-78", "angelycamilcg@ufps.edu.co",
+			  "3219810616", "Juridico", "Regimen", 1), new
+			  Factura("CUFEASDSDFASDFA3424534", new GregorianCalendar(2021, 11,
+			  11).getTime(), new GregorianCalendar(2025, 11, 11).getTime(), 3, 45, (byte)
+			  1, "SebastianSanchez", 1, 1, 1), new DetalleFactura(1, "CUFEDSA", 1, 1), new
+			  RangoNumeracion(24551, new Date(), 1876401818, 24551, 50000, 20001, "FE"));
+			 
 
 		/*
 		 * String cufe ="";
